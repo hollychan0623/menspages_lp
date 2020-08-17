11503b45b9740adc004d09c2d403b5072e9b8271
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-08-17 12:50:11 +0900

    メンズページ設定

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+$(function(){
+    function animation(){
+      $('.fadeInUp').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
+  $(function(){
+    function animation(){
+      $('.fadeInUpright').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
+  $(function(){
+    function animation(){
+      $('.items').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
+  $(function(){
+    function animation(){
+      $('.wallet_items').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
+//  $(document). ready(function(){
+//    $('.items').fadeIn(1500);
+//  })
+
+$(function() {
+  var $allMsg = $('#title');
+  var $wordList = $('#title').html().split("");
+  $('#title').html("");
+  $.each($wordList, function(idx, elem) {
+      var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+      newEL.appendTo($allMsg);
+      newEL.delay(idx * 100);
+      newEL.animate({ opacity: 1 }, 2000);
+  });
+});
