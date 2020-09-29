959e19d5266cec8d0826ea48a7e014465cb1ac25
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-09-29 10:07:00 +0900

    topへ戻るボタンを追加
    背景青色にして、レディースと同じCSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,23 @@ $(function() {
     });
   });
 })(jQuery);
+
+$(function() {
+  var topBtn = $('#page-top');    
+  topBtn.hide();
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          topBtn.fadeIn();
+      } else {
+          topBtn.fadeOut();
+      }
+  });
+  //スクロールしてトップ
+  topBtn.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500);
+      return false;
+  });
+});
